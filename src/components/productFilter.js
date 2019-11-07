bb5dff36544b54a2e18bb0bccb12bc01cf3f1578
Eduardo Rodriguez <rodriguez.ambriz@outlook.com>
2019-11-07 02:08:33 -0600

    font weight

--- src/components/productFilter.js
+++ src/components/productFilter.js
@@ -42,7 +42,7 @@ const ProductFilter = ({ products, filterByPrice }) => {
             Price{" "}
             <span
               style={{
-                fontWeight: 600,
+                fontWeight: 800,
                 fontSize: "1.25em",
                 marginLeft: "5px",
               }}
